6054fe1dc34b0902f0f93bc638617884eeb2f373
yangjh <yangjh@mz.co.kr>
2020-12-16 18:38:12 +0900

    properteis add

--- src/main/java/com/mongodb/migtool/MigrationToolApplication.java
+++ src/main/java/com/mongodb/migtool/MigrationToolApplication.java
@@ -9,13 +9,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.bson.BsonObjectId;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -23,19 +21,18 @@ import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.BasicDBObject;
 import com.mongodb.ConnectionString;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.FindIterable;
@@ -46,42 +43,83 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.InsertOneModel;
 import com.mongodb.client.model.Sorts;
-import com.mongodb.migtool.model.Stage1ChangedProfileSetup;
-import com.mongodb.migtool.model.Stage1PresentProfilesSetup;
-import com.mongodb.migtool.model.Stage1ProfileInfo;
-import com.mongodb.migtool.model.Stage2ChangedProfileSetup;
-import com.mongodb.migtool.model.Stage2PresentProfilesSetup;
-import com.mongodb.migtool.model.Stage2ProfileInfo;
-import com.mongodb.migtool.model.TestInsert;
 import com.mongodb.migtool.util.MigUtil;
 
 
 @SpringBootApplication
 // 자동으로 MongoAutoConfiguration 이나 MongoDataAutoConfiguration 이 실행 되지 않도록 방지
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableConfigurationProperties
 public class MigrationToolApplication {
+	
+	@Value("${dbinfo.oracleip}") String oracleip;
+	@Value("${dbinfo.oracleport}") String oracleport;
+	@Value("${dbinfo.oracleid}") String oracleid;
+	@Value("${dbinfo.oraclepw}") String oraclepw; 
+	@Value("${dbinfo.oracledatabasename}") String oracledatabasename;
+	@Value("${dbinfo.mongouri}") String mongouri ;
+	@Value("${dbinfo.mongodatabasename}") String mongodatabasename ; 
+	@Value("${dbinfo.mongodatabaseargs}") String mongodatabaseargs ; 
 
 	private static ArrayList<Map<String,String>> tableList = new ArrayList<Map<String,String>>();
-
 	private static Logger logger = LoggerFactory.getLogger(MigrationToolApplication.class);
-
-	private final static SimpleDateFormat stdDate = new SimpleDateFormat("yyyyMMdd");
-
 	private static MongoClient mongoClient;
 	private static Connection conn;
+	private static String MONGO_DATABASE_NAME;
 	
-	private final static String OracleDatabaseName = "xe";
-	private final static String MongoDataBaseName = "forum";
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationToolApplication.class, args);
+	}
 	
-	
-	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			logger.info("--------------------------------------------------------------------------------");
+			logger.info("Start MigrationToolApplication");
+			logger.info("--------------------------------------------------------------------------------");
+
+			System.out.println("******************************************** ");
+		    System.out.println("***** application.yml properties info  ***** ");
+			Map<String,String> connectionParam = new HashMap<String,String>();
+			connectionParam.put("oracleip", oracleip);
+			connectionParam.put("oracleport", oracleport);
+			connectionParam.put("oracleid", oracleid);
+			connectionParam.put("oraclepw", oraclepw);
+			connectionParam.put("oracledatabasename", oracledatabasename);
+			connectionParam.put("mongouri", mongouri);
+			connectionParam.put("mongodatabasename", mongodatabasename);
+			connectionParam.put("mongodatabaseargs", mongodatabaseargs);
+			System.out.println("ORACLE_IP : "+oracleip);
+			System.out.println("ORACLE_PORT : "+oracleport);
+			System.out.println("ORACLE_ID : "+oracleid);
+			System.out.println("ORACLE_PW : "+oraclepw);
+			System.out.println("ORACLE_DATABASE_NAME : "+oracledatabasename);
+			System.out.println("ORACLE_MONGO_URI : "+mongouri);
+			System.out.println("ORACLE_MONGO_DATABASE : "+ mongodatabasename);
+			System.out.println("ORACLE_MONGO_URI_PARAM : "+ mongodatabaseargs);
+			System.out.println("***** application.yml properties info ***** ");
+			System.out.println("******************************************** ");
+			MONGO_DATABASE_NAME = mongodatabasename;
+			
+			batchInitConnection(connectionParam);
+			batchInitTargetTableSetting();
+			// oracleDummyData();
+			batchWorker();
+
+			logger.info("--------------------------------------------------------------------------------");
+			logger.info("MigrationToolApplication Finished");
+			logger.info("--------------------------------------------------------------------------------");
+
+			conn.close();
+			mongoClient.close();
+
+		};
+	}
+
 	
 	private static void oracleToMongoInsert(String tableName , String whereQuery , String insertMongoCollectionName) throws Exception {
-		
 		StringBuilder sqlString = new StringBuilder()
 				.append("SELECT * FROM  ").append(tableName).append(" where 1=1 and ").append(whereQuery);
-		
-
 		try {
 
 			PreparedStatement psmt = conn.prepareStatement(sqlString.toString());
@@ -94,7 +132,7 @@ public class MigrationToolApplication {
 
 			HashMap buffer = null;
 
-			MongoDatabase mongoDatabase = mongoClient.getDatabase("forum");
+			MongoDatabase mongoDatabase = mongoClient.getDatabase(MONGO_DATABASE_NAME);
 			MongoCollection mongoCollection = mongoDatabase.getCollection(insertMongoCollectionName); //"test_collection"
 			
 			BulkWriteResult bulkWriteResult = null;
@@ -110,7 +148,7 @@ public class MigrationToolApplication {
 					int columnTypeCode =  rsmd.getColumnType(idx) ;
 				    buffer.put(key,  MigUtil.converTypeCasting( columnTypeCode , rs.getObject(idx)));
 				}
-				Document remappingMap = convertTargetDocumentVO(buffer,insertMongoCollectionName );
+				Document remappingMap = convertTargetDocumentVO(buffer,insertMongoCollectionName , mongoDatabase );
 				insertOneModel = new InsertOneModel(remappingMap);
 				insertDocuments.add(insertOneModel);
 				insertCount++;
@@ -131,11 +169,6 @@ public class MigrationToolApplication {
 	}
 	
 
-
-	
-
-	
-
 	public static void batchWorker() {
 		boolean isResult = false;
 		try {
@@ -153,14 +186,16 @@ public class MigrationToolApplication {
 
 	}
 
-	public static void batchInitConnection(){
+	public static void batchInitConnection(Map<String, String> connectionParam){
 
 		// ---------------------------------------------------------------------
 		// Oracle Connect
 		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url="jdbc:oracle:thin:@localhost:1521:"+OracleDatabaseName;
-		String user="system";
-		String pwd="oracle";
+		String url = "jdbc:oracle:thin:@"+connectionParam.get("oracleip")+":"+connectionParam.get("oracleport")+":"+connectionParam.get("oracledatabasename");
+		System.out.println("oracle Url : "+url);
+		String user = connectionParam.get("oracleid");
+		String pwd = connectionParam.get("oraclepw");
+		
 
 		conn = getConnection(driver,url,user,pwd);
 
@@ -168,10 +203,13 @@ public class MigrationToolApplication {
 		// MongoDB Connect with MongoDB URI format
 		ConnectionString connectionString = new ConnectionString(
 				//"mongodb://admin:admin@localhost:27017,localhost:27018,localhost:27019/test?authSource=admin&replicaSet=replset"
-				"mongodb://jaehyun:jaehyun@localhost:27017/"+MongoDataBaseName+"?authMechanism=SCRAM-SHA-1"
+				// "mongodb://jaehyun:jaehyun@localhost:27017/"+connectionParam.get("ora")+"?authMechanism=SCRAM-SHA-1"
+				connectionParam.get("mongouri") + connectionParam.get("mongodatabasename") + "?"  +  connectionParam.get("mongodatabaseargs")
 		);
 		
-
+		System.out.println("mongo connection info : "+connectionString);
+		
+		
 		// POJO Object를 등록
 		CodecRegistry pojoCodecRegistry = fromProviders(
 				PojoCodecProvider.
@@ -218,15 +256,14 @@ public class MigrationToolApplication {
 		return conn;
 	}
 	
-	
-
+   
 	public  void batchInitTargetTableSetting () throws Exception{
 		
 		ArrayList<Map<String,String>> list = new ArrayList<Map<String,String>>();
 		Map<String,String> map1 = new HashMap<String, String>();
 		map1.put("tableName", "TEST_TABLE");
-		map1.put("whereQuery", "test_id <=1 ");
-		map1.put("targetCollection","stage1PresentProfileSetup");
+		map1.put("whereQuery", "SEQ <=1 ");
+		map1.put("targetCollection","stage1ProfileInfo");
 		list.add(map1);
 		
 //		Map<String,String> map2 = new HashMap<String, String>();
@@ -242,36 +279,10 @@ public class MigrationToolApplication {
 		tableList = list;
 	}
 	
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			logger.info("--------------------------------------------------------------------------------");
-			logger.info("Start MigrationToolApplication");
-			logger.info("--------------------------------------------------------------------------------");
-
-			batchInitConnection();
-			batchInitTargetTableSetting();
-			// oracleDummyData();
-			batchWorker();
-
-			logger.info("--------------------------------------------------------------------------------");
-			logger.info("MigrationToolApplication Finished");
-			logger.info("--------------------------------------------------------------------------------");
-
-			conn.close();
-			mongoClient.close();
-
-		};
-	}
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(MigrationToolApplication.class, args);
-	}
 	
 	
 	
-	public static Document convertTargetDocumentVO(HashMap buffer, String insertMongoCollectionName ) {
+	public static Document convertTargetDocumentVO(HashMap buffer, String insertMongoCollectionName , MongoDatabase mongoDatabase ) {
 		
 		Document dc = new Document();		
 		Map<String,Object> idSet = new HashMap<String,Object>();
@@ -299,7 +310,7 @@ public class MigrationToolApplication {
 			
 		}else if("stage1PresentProfileSetup".equals(insertMongoCollectionName)) {
 		  
-		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName )) );
+		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName , mongoDatabase )) );
 		    idSet.put("category", buffer.get("CATEGORY").toString()  );
 		    
 			
@@ -310,14 +321,14 @@ public class MigrationToolApplication {
 			dc.append("metaDataVersion",buffer.get("METADATA_VERSION").toString());
 			dc.append("savedTime",savedTimeVal);
 			dc.append("fileName",buffer.get("FILENAME").toString());
-			dc.append("setupFile", !"50".equals(buffer.get("CATEGORY").toString()) ? MigUtil.base64Decoding(buffer.get("FILENAME").toString())  :  buffer.get("FILENAME").toString());
+			// dc.append("setupFile", !"50".equals(buffer.get("CATEGORY").toString()) ? MigUtil.base64Decoding(buffer.get("FILENAME").toString())  :  buffer.get("FILENAME").toString());
 			dc.append("createdDate",buffer.get("RGST_DTM").toString());
 			//dc.append("",);
 
 		
 		}else if("stage2PresentProfileSetup".equals(insertMongoCollectionName)) {
 	   	   
-		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName )) );
+		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName , mongoDatabase )) );
 		    idSet.put("category", buffer.get("CATEGORY").toString()  );
 		    
 			
@@ -332,7 +343,7 @@ public class MigrationToolApplication {
 			dc.append("createdDate",buffer.get("RGST_DTM").toString());
 		}else if("stage1ChangedProfileSetup".equals(insertMongoCollectionName)) {
 
-		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName )) );
+		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName  , mongoDatabase )) );
 		    idSet.put("category", buffer.get("CATEGORY").toString()  );
 		    
 			savedTimeVal.put("utc",  buffer.get("UTC_OFFSET_DATETIME").toString()  );
@@ -346,7 +357,7 @@ public class MigrationToolApplication {
 			dc.append("createdDate",buffer.get("RGST_DTM").toString());
 			
 		}else if("stage2ChangedProfileSetup".equals(insertMongoCollectionName)) {
-		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName )) );
+		    idSet.put("profileInfoId", new ObjectId( getMasterObjectId( buffer.get("SEQ").toString() , insertMongoCollectionName , mongoDatabase )) );
 		    idSet.put("category", buffer.get("CATEGORY").toString()  );
 		    
 			savedTimeVal.put("utc",  buffer.get("UTC_OFFSET_DATETIME").toString()  );
@@ -366,13 +377,10 @@ public class MigrationToolApplication {
 		return dc;
 	}
 
-	public static String getMasterObjectId (String seq , String insertMongoCollectionName ) {
+	public static String getMasterObjectId (String seq , String insertMongoCollectionName , MongoDatabase mongoDatabase ) {
 		Bson filter = Filters.eq("seq",seq);
 		Bson sort = Sorts.descending("seq");
-		
-		MongoDatabase mongoDatabase = mongoClient.getDatabase(MongoDataBaseName);
 		MongoCollection mongoCollection = null;
-	
 		if(insertMongoCollectionName.indexOf("1") > 0) {
 			mongoCollection = mongoDatabase.getCollection("stage1ProfileInfo");
 		}else {
